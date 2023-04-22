d20d845ede461b826e08f3790db73c79d29799d4
hoongel1004 <75716280+hoongel1004@users.noreply.github.com>
2023-04-22 15:05:27 -0400

    0422yujeong

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -16,6 +16,9 @@ function ready() {
     */
 }
 function deleteRecipe(event) {
+    if(!confirm('Are you sure you want to delete this recipe?')){
+        return false;
+    }
     const db = getFirestore();
     db.collection('Recipes').doc(event).delete();
     var buttonClicked = event.target
